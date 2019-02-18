266831c8e7365043f00bbeffaabf087c06c1d726
diana4545 <diana4545@outlook.com>
2019-02-18 10:28:24 +0000

    with new table

--- model.php
+++ model.php
@@ -20,49 +20,49 @@ if(isset($post['how_much'])){
 
   //extract data from the post
   //set POST variables
-  $url = 'https://sheets.googleapis.com/v4/spreadsheets?key=AIzaSyCYRKWBH759HZen9Q6KVvnEwXl74iP9Szg';
-
-  // A sample PHP Script to POST data using cURL
-  // Data in JSON format
-
-  $data = array(
-    'first_name' => $first_name,
-    'last_name' => $last_name,
-    'phone_number' => $phone_number,
-    'email_address' => $email_address,
-    'business_name' => $business_name,
-    'how_much' => $how_much,
-    'how_soon' => $how_soon,
-    'years_business' => $years_business,
-    'monthly_revenue' => $monthly_revenue,
-    'industry' => $industry
-  );
-
-  $payload = json_encode($data);
-
-  //var_dump($payload);exit;
-
-  // Prepare new cURL resource
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-
-  // Set HTTP Header for POST request
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Accept: application/json',
-      'Content-Type: application/json',
-      'Content-Length: ' . strlen($payload))
-  );
-
-  // Submit the POST request
-  $result = curl_exec($ch);
-
-  var_dump($result);exit;
-
-  // Close cURL session handle
-  curl_close($ch);
+  // $url = 'https://sheets.googleapis.com/v4/spreadsheets?key=AIzaSyCYRKWBH759HZen9Q6KVvnEwXl74iP9Szg';
+  //
+  // // A sample PHP Script to POST data using cURL
+  // // Data in JSON format
+  //
+  // $data = array(
+  //   'first_name' => $first_name,
+  //   'last_name' => $last_name,
+  //   'phone_number' => $phone_number,
+  //   'email_address' => $email_address,
+  //   'business_name' => $business_name,
+  //   'how_much' => $how_much,
+  //   'how_soon' => $how_soon,
+  //   'years_business' => $years_business,
+  //   'monthly_revenue' => $monthly_revenue,
+  //   'industry' => $industry
+  // );
+  //
+  // $payload = json_encode($data);
+  //
+  // //var_dump($payload);exit;
+  //
+  // // Prepare new cURL resource
+  // $ch = curl_init($url);
+  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  // curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+  // curl_setopt($ch, CURLOPT_POST, true);
+  // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+  //
+  // // Set HTTP Header for POST request
+  // curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+  //     'Accept: application/json',
+  //     'Content-Type: application/json',
+  //     'Content-Length: ' . strlen($payload))
+  // );
+  //
+  // // Submit the POST request
+  // $result = curl_exec($ch);
+  //
+  // var_dump($result);exit;
+  //
+  // // Close cURL session handle
+  // curl_close($ch);
 
 }
 else {
---
+++ table.php
@@ -0,0 +1,144 @@
+<?php
+//Database Connect
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "monetcap";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+}
+$table = 'request';
+$result = array();
+if($table == 'request'){
+	$sql = "SELECT * FROM request";
+	$result = $conn->query($sql);
+}
+
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Table V02</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+
+	<div class="limiter">
+		<div class="container-table100">
+			<div class="wrap-table100">
+					<div class="table">
+
+						<div class="row header">
+							<div class="cell">
+								First Name
+							</div>
+							<div class="cell">
+								Last Name
+							</div>
+							<div class="cell">
+								Email
+							</div>
+							<div class="cell">
+								Phone
+							</div>
+							<div class="cell">
+								Business Name
+							</div>
+							<div class="cell">
+								How Much?
+							</div>
+							<div class="cell">
+								How Soon?
+							</div>
+							<div class="cell">
+								Years in busienss
+							</div>
+							<div class="cell">
+								Monthly Revenue
+							</div>
+							<div class="cell">
+								Industry
+							</div>
+						</div>
+						<?php
+						 	if ($result->num_rows > 0) {
+							    // output data of each row
+							    while($row = $result->fetch_assoc()) {
+										echo '
+										<div class="row">
+											<div class="cell" data-title="First Name">'
+												. $row['first_name']
+											.'</div>
+											<div class="cell" data-title="Last Name">'
+												. $row['last_name']
+											.'</div>
+											<div class="cell" data-title="Email">'
+												. $row['email_address']
+											.'</div>
+											<div class="cell" data-title="Phone">'
+												. $row['phone_number']
+											.'</div>
+											<div class="cell" data-title="Business Name">'
+												. $row['business_name']
+											.'</div>
+											<div class="cell" data-title="How Much?">'
+												. $row['how_much']
+											.'</div>
+											<div class="cell" data-title="How Soon?">'
+												. $row['how_soon']
+											.'</div>
+											<div class="cell" data-title="Years in Business">'
+												. $row['years_business']
+											.'</div>
+											<div class="cell" data-title="Monthly Revenue">'
+												. $row['monthly_revenue']
+											.'</div>
+											<div class="cell" data-title="Industry">'
+												. $row['industry']
+											.'</div>
+											</div>';											
+							    }
+							}
+						?>
+					</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
